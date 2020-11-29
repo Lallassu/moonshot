66899543712706c484e7297d25798a38f922855e
Magnus Persson <nergal@nergal.se>
2020-11-29 15:44:43 +0100

    Fix dist from missing files

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ dist: build
 	mkdir moonshot_game
 	cp moonshot moonshot_game/
 	cp -r assets moonshot_game/
+	cp gameconf.json moonshot_game/
 	tar cvfz moonshot.tar.gz moonshot_game
 	rm -rf moonshot_game
 	
--- assets/shaders/particle.fs
+++ assets/shaders/particle.fs
@@ -7,12 +7,6 @@ out vec4 color;
 
 void main() 
 {
-    //float d = distance(gl_FragCoord.xy, pos);
     color = particlecolor/0xFFFF;
-    //if(d <= 0) {
-    //    d = 0.1;
-    //}
-    //float a = d/size;
-    //color.a = a;
 }
 
